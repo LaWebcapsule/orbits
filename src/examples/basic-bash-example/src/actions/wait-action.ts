d0179ad1a05371d47792e762489c74854114a579
adussarps <adussarps@gmail.com>
2023-01-20 10:49:27 +0100

    more logs

--- src/examples/basic-bash-example/src/actions/wait-action.ts
+++ src/examples/basic-bash-example/src/actions/wait-action.ts
@@ -1,7 +1,8 @@
 import { Action, ActionState } from '@wbce/orbits-core'
 
 export class WaitAction extends Action {
-    static permanentName: 'wait-action'
+    static permanentName: 'wait-action';
+
 
     IArgument: {
         waitTime: number
@@ -12,20 +13,23 @@ export class WaitAction extends Action {
     }
 
     init(){
-        this.bag.initTime = new Date().getUTCDate();
+        this.bag.initTime = new Date().getTime();
         console.log("Wait action initialized with: "+ this.bag.initTime);
+        this.defaultDelay = this.argument.waitTime ? this.argument.waitTime / 1000 : 1;
         return Promise.resolve();
     }
 
     main() {
-        var currentTime = new Date().getUTCDate();
+        var currentTime = new Date().getTime();
         if (this.argument.waitTime > 0) {
             if(currentTime - this.argument.waitTime > this.bag.initTime){
+                console.log("Wait action over");
                 return ActionState.SUCCESS;
             }
         }else{
             console.log("Using default wait time: 1000ms");
             if(currentTime - 1000 > this.bag.initTime){
+                console.log("Wait action over");
                 return ActionState.SUCCESS;
             }
         }
