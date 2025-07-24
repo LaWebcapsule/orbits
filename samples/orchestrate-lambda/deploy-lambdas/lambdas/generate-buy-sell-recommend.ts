006d736e4b5f4184044d19146ca5b6c0c19bc92b
Tom Marcuzzi <tom@webcapsule.io>
2025-07-24 15:52:02 +0200

    Format latest sample and doc

--- samples/orchestrate-lambda/deploy-lambdas/lambdas/generate-buy-sell-recommend.ts
+++ samples/orchestrate-lambda/deploy-lambdas/lambdas/generate-buy-sell-recommend.ts
@@ -1,22 +1,21 @@
 interface Event {
-  body: string;
+    body: string;
 }
 
 interface LambdaResponse {
-  statusCode: number;
-  body: string;
+    statusCode: number;
+    body: string;
 }
 
 export const handler = async (event: Event): Promise<LambdaResponse> => {
+    const body = JSON.parse(event.body);
+    const { stock_price } = body;
 
-  const body = JSON.parse(event.body);
-  const { stock_price } = body;
+    console.log(`stock_price: ${stock_price}`);
+    const recommendation = stock_price > 50 ? 'sell' : 'buy';
 
-  console.log(`stock_price: ${stock_price}`);
-  const recommendation = stock_price > 50 ? 'sell' : 'buy';
-
-  return {
-    statusCode: 200,
-    body: recommendation
-  };
+    return {
+        statusCode: 200,
+        body: recommendation,
+    };
 };
