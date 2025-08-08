5869af12ad8006a3297a96c8ef2cd1a526f56ed7
Tom Marcuzzi <tom@webcapsule.io>
2025-08-08 11:44:49 +0200

    feat: Add database logger

--- core/actions/index.ts
+++ core/actions/index.ts
@@ -4,9 +4,11 @@ export * from './src/action-manager.js';
 export * from './src/error/error.js';
 export { errorCodes } from './src/error/errorcodes.js';
 export * from './src/models/action.js';
+export * from './src/models/log.js';
 export * from './src/runtime/action-kind.js';
 export * from './src/runtime/action-runtime.js';
 export type { RuntimeDb as AppDb } from './src/runtime/db-connection.js';
+export { databaseLogger, defaultLogger } from './src/runtime/logger.js';
 // order is important between workflow and coalescing manager
 // prettier-ignore
 export * from './src/workflow-manager.js';
