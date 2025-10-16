83c449d91fb9da96486b4c0f82058a2dff13c918
Tom Marcuzzi <tom@webcapsule.io>
2025-10-16 16:01:04 +0200

    cli - copy logs on selection

--- cli/src/viewer/utils.ts
+++ cli/src/viewer/utils.ts
@@ -1,4 +1,5 @@
 import { ActionState } from '@orbi-ts/core';
+import ansiStyles from 'ansi-styles';
 import colors from 'colors';
 
 export const ACTION_STATE_FORMAT = new Map<
@@ -7,13 +8,22 @@ export const ACTION_STATE_FORMAT = new Map<
 >([
     [ActionState.UNKNOWN, { short: '?', full: 'UNKNOWN', color: 'grey' }],
     [ActionState.SLEEPING, { short: 'z', full: 'SLEEPING', color: 'grey' }],
-    [ActionState.EXECUTING_MAIN, { short: '…', full: 'EXECUTING MAIN', color: 'cyan' }],
-    [ActionState.IN_PROGRESS,  { short: '…', full: 'IN PROGRESS', color: 'cyan' }],
+    [
+        ActionState.EXECUTING_MAIN,
+        { short: '…', full: 'EXECUTING MAIN', color: 'cyan' },
+    ],
+    [
+        ActionState.IN_PROGRESS,
+        { short: '…', full: 'IN PROGRESS', color: 'cyan' },
+    ],
     [ActionState.PAUSED, { short: '⏸', full: 'PAUSED', color: 'grey' }],
     [ActionState.SUCCESS, { short: '✔', full: 'SUCCESS', color: 'green' }],
     [ActionState.ERROR, { short: '✘', full: 'ERROR', color: 'red' }],
-    [ActionState.CLOSED, {short: 'X', full: 'CLOSED', color: 'grey' }],
-    [ActionState.REVERTING, { short: '↩', full: 'REVERTING', color: 'yellow' }],
+    [ActionState.CLOSED, { short: 'X', full: 'CLOSED', color: 'grey' }],
+    [
+        ActionState.REVERTING,
+        { short: '↩', full: 'REVERTING', color: 'yellow' },
+    ],
     [ActionState.REVERTED, { short: '✔', full: 'REVERTED', color: 'yellow' }],
 ]);
 
@@ -56,4 +66,48 @@ export function generatePrettyActionState(
         default:
             return colorFunc(short);
     }
+}
+
+export const HIGHLIGHT_OPEN = ansiStyles.bgWhiteBright.open;
+export const HIGHLIGHT_CLOSE = ansiStyles.bgWhiteBright.close;
+
+/**
+ * Highlight string from index startCol to endCol excluded
+ */
+export function highlight(str: string, startCol: number, endCol: number) {
+    let result = '';
+    let buffer = '';
+    let inAnsi = false;
+    let inHighlight = false;
+    let charIdx = 0;
+
+    for (const ch of str) {
+        // start of ANSI code
+        if (ch === '\x1b') {
+            inAnsi = true;
+            buffer = ch;
+            continue;
+        }
+        if (inAnsi) {
+            buffer += ch;
+            // end of ANSI code
+            if (ch === 'm') {
+                result += buffer;
+                inAnsi = false;
+            }
+            continue;
+        }
+        if (charIdx === startCol && !inHighlight) {
+            result += HIGHLIGHT_OPEN;
+            inHighlight = true;
+        }
+        if (charIdx === endCol && inHighlight) {
+            result += HIGHLIGHT_CLOSE;
+            inHighlight = false;
+        }
+        result += ch;
+        charIdx++;
+    }
+    if (inHighlight) result += HIGHLIGHT_CLOSE;
+    return result;
 }
