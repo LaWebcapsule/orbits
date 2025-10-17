a2c1163e2bc3961662a5d78884a6a3265c8bef9e
WBCE_staging <WBCE_builder@staging>
2025-10-17 10:47:54 +0200

    clean

--- samples/orchestrate-lambda/src/orbits/orbi.ts
+++ samples/orchestrate-lambda/src/orbits/orbi.ts
@@ -2,13 +2,10 @@ import { Action, ActionRuntime, ActionState } from '@orbi-ts/core';
 import { TradingWorkflow } from './workflows/trading';
 
 ActionRuntime.activeRuntime.waitForBootstrap.then(async () => {
-    if(process.env['main'] === 'main'){
-        const workflow = new TradingWorkflow();
-        await workflow.save();
-        await Action.trackActionAsPromise(workflow, [
-            ActionState.SUCCESS,
-            ActionState.ERROR,
-        ]);
-        
-    }
+    const workflow = new TradingWorkflow();
+    await workflow.save();
+    await Action.trackActionAsPromise(workflow, [
+        ActionState.SUCCESS,
+        ActionState.ERROR,
+    ]);
 });
